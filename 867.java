46492fec024c833ce3c8ee5427aea15a8cd1a037
Sivanatchathira14 <natchathiram98@gmail.com>
2026-09-25 20:40:12 +0530

    Add transpose method to Solution class
    Implement matrix transposition method.
    Example 1:
    
    Input: matrix = [[1,2,3],[4,5,6],[7,8,9]]
    Output: [[1,4,7],[2,5,8],[3,6,9]]
    Example 2:
    
    Input: matrix = [[1,2,3],[4,5,6]]
    Output: [[1,4],[2,5],[3,6]]

---
+++ 867.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int[][] transpose(int[][] a) {
+        int b[][]=new int[a[0].length][a.length];
+        for(int i=0;i<a.length;i++){
+            for(int j=0;j<a[0].length;j++){
+                b[j][i]=a[i][j];
+            
+            }
+        }
+        return b;
+        
+    }
+}
